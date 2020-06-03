7825f3b2708e72d0b592f0589cac75aa4e9828ff
Chris Buckley <chris@cmbuckley.co.uk>
2020-06-03 01:44:53 +0100

    Actually support arguments

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ const fs = require('fs'),
       ced = require('ced'),
       Iconv = require('iconv').Iconv;
 
-const file = fs.readFileSync(0),
-      iconv = new Iconv(ced(file), 'utf8');
+let files = process.argv.slice(2);
 
-// convert file contents to UTF-8
-process.stdout.write(iconv.convert(file));
+(files.length ? files : [0]).forEach(function (file) {
+    let data;
+
+    try {
+        data = fs.readFileSync(file);
+    } catch (err) {
+        if (err.code == 'ENOENT') {
+            return console.error(err.message);
+        }
+
+        throw err;
+    }
+
+    // convert file contents to UTF-8
+    const iconv = new Iconv(ced(data), 'utf8');
+    process.stdout.write(iconv.convert(data));
+});
